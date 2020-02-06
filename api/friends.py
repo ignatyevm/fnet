85a355777f777410ab5c724730b0f57a69deeb7f
polyndrom <polyndromdev@gmail.com>
2020-02-06 20:47:29 +1000

    Fix friends

--- api/friends.py
+++ api/friends.py
@@ -14,17 +14,15 @@ from database import database_cursor
 friends = Blueprint('friends', __name__)
 
 
-@friends.route('/', methods=['GET'])
-def get_friends():
-    token = validate(request.args, [FieldValidator('token').required()])
-    user_id = validate_token(token)
+@friends.route('/<int:user_id>', methods=['GET'])
+def get_friends(user_id):
     database_cursor.execute(
         'SELECT sender_id, receiver_id FROM "FriendRequest" WHERE (sender_id = %s OR receiver_id = %s) AND is_accepted = true',
         (user_id, user_id,))
     result = database_cursor.fetchall()
     friends_ids = []
     for pair in result:
-        sender_id, receiver_id = pair
+        sender_id, receiver_id = pair.values()
         if sender_id == user_id:
             friends_ids.append(receiver_id)
         else:
@@ -34,7 +32,7 @@ def get_friends():
 
 @friends.route('/<int:friend_id>', methods=['DELETE'])
 def delete_friend(friend_id):
-    token = validate(request.args, [FieldValidator('token').required()])
+    token = validate(request.get_json(), [FieldValidator('token').required()])
     user_id = validate_token(token)
     database_cursor.execute(
         'DELETE FROM "FriendRequest" WHERE '
@@ -45,7 +43,7 @@ def delete_friend(friend_id):
 
 @friends.route('/<int:receiver_id>', methods=['POST'])
 def send_friend_request(receiver_id):
-    token = validate(request.args, [FieldValidator('token').required()])
+    token = validate(request.get_json(), [FieldValidator('token').required()])
     sender_id = validate_token(token)
     database_cursor.execute('INSERT INTO "FriendRequest"(sender_id, receiver_id) VALUES (%s, %s)', (sender_id, receiver_id,))
     return ResponseManager.success()
@@ -53,7 +51,7 @@ def send_friend_request(receiver_id):
 
 @friends.route('/<int:sender_id>', methods=['PUT'])
 def accept_friend_request(sender_id):
-    token = validate(request.args, [FieldValidator('token').required()])
+    token = validate(request.get_json(), [FieldValidator('token').required()])
     receiver_id = validate_token(token)
     database_cursor.execute('UPDATE "FriendRequest" SET is_accepted = true WHERE sender_id = %s AND receiver_id = %s', (sender_id, receiver_id,))
     return ResponseManager.success()
